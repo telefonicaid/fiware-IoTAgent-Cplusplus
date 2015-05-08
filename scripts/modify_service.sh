820b488a8027a08c0d7758569c1dae428940ddcf
rbl219 <e.idas1@tid.es>
2015-05-08 06:11:25 -0700

    FEATURE IDAS-20473: Generate example curl script for IotMager

--- scripts/modify_service.sh
+++ scripts/modify_service.sh
@@ -1,6 +1,6 @@
-curl -X PUT 'http://127.0.0.1:8080/iot/services/TestService?apikey=apikey3&resource=/iot/test' \
+curl -X PUT 'http://127.0.0.1:8080/iot/services/testservice?apikey=apikey3&resource=/iot/test' \
 -i \
 -H "Content-Type: application/json" \
--H "Fiware-Service: TestService" \
--H "Fiware-ServicePath: /TestSubservice" \
+-H "Fiware-Service: testservice" \
+-H "Fiware-ServicePath: /testsubservice" \
 -d '{ "apikey": "NEWapikey", "token": "token2", "cbroker": "http://127.0.0.1:1026", "entity_type": "thing", "resource": "/iot/test" }'
