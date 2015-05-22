36a6d264d56a0ede7cc3b8c19f772d78698e6fe2
jmab20 <jose.miguel-arigita@tecsidel.es>
2015-05-22 09:28:13 +0200

    FIX IDAS-20500: with a mongo driver installed by default, iotagent doe not compile

--- cmake/FindMongoDB.cmake
+++ cmake/FindMongoDB.cmake
@@ -10,8 +10,9 @@
 
 find_path(MongoDB_INCLUDE_DIR mongo/client/dbclient.h
                ${IOTAGENT_DEPENDENCIES_PATHS}
-               $ENV{MongoDB_ROOT}/lib
-               ${MongoDB_ROOT}/lib)
+               $ENV{MongoDB_ROOT}/include
+               ${MongoDB_ROOT}/include
+               NO_DEFAULT_PATH)
 if (MONGODB_USE_STATIC_LIBS STREQUAL "ON")
 set(_mongodb_LIB_NAME "libmongoclient.a")
 else()
