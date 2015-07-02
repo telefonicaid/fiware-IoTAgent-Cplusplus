0ee2b08b6073713cff0110d101c57570d9c608c0
jmab20 <jose.miguel-arigita@tecsidel.es>
2015-07-02 08:56:05 +0200

    add mongo pool to release 1.0.2

--- src/util/mongo_connection.cc
+++ src/util/mongo_connection.cc
@@ -33,36 +33,31 @@
 #include "store_const.h"
 #include "alarm.h"
 
+#define SIZE_POOL 10
+#define MAX_SIZE_POOL 1000
+
 namespace iota {
 extern std::string logger;
 }
 
+iota::MongoConnection* iota::MongoConnection::pinstance = 0;// Inicializar el puntero
+
 const int MONGO_TIMEOUT = 3;
 
-iota::MongoConnection::MongoConnection():m_logger(PION_GET_LOGGER(
-        iota::logger)) {
-  _conex_pool = NULL;
+iota::MongoConnection::MongoConnection():
+       _conex_pool(SIZE_POOL),
+       m_logger(PION_GET_LOGGER(iota::logger)) {
+
   try{
     reconnect();
   }catch(std::exception exc){
-    PION_LOG_ERROR(m_logger, "error in MongoConnection " << exc.what() );
-  }
-}
-
-iota::MongoConnection::MongoConnection(const std::string& ip,
-                                       const std::string& puerto,
-                                       const std::string& dbname,
-                                       const std::string& user,
-                                       const std::string& pwd) {
-  try{
-    reconnect(ip, puerto, dbname, user, pwd);
-  }catch(std::exception exc){
-    PION_LOG_ERROR(m_logger, "error in MongoConnection " << exc.what() );
+    IOTA_LOG_ERROR(m_logger, "error in MongoConnection " << exc.what() );
   }
 }
 
 
 void iota::MongoConnection::reconnect(){
+  boost::mutex::scoped_lock lock(_m);
   std::string db_info;
   try {
     const JsonValue& storage=
@@ -73,7 +68,7 @@ void iota::MongoConnection::reconnect(){
     if (storage.HasMember(iota::store::types::TYPE.c_str())) {
       std::string type = storage[iota::store::types::TYPE.c_str()].GetString();
       if (type.compare(iota::types::CONF_FILE_MONGO) != 0) {
-        PION_LOG_DEBUG(m_logger,
+        IOTA_LOG_DEBUG(m_logger,
                        "no config for mongodb");
         return;
       }
@@ -85,17 +80,31 @@ void iota::MongoConnection::reconnect(){
 
     if (storage.HasMember(iota::store::types::HOST.c_str())) {
       _host.assign(storage[iota::store::types::HOST.c_str()].GetString());
-      PION_LOG_DEBUG(m_logger, "in storage host " << _host);
+      IOTA_LOG_DEBUG(m_logger, "in storage host " << _host);
     }
     else {
-      PION_LOG_DEBUG(m_logger,
+      IOTA_LOG_DEBUG(m_logger,
                      "in storage no host defined, using localhost by default");
       _host.assign("127.0.0.1");
     }
 
+    int pool_size = SIZE_POOL;
+    if (storage.HasMember(iota::store::types::POOL_SIZE.c_str())) {
+      std::string pool_sizeSTR = storage[iota::store::types::POOL_SIZE.c_str()].GetString();
+      try {
+        pool_size = boost::lexical_cast<int>(pool_sizeSTR);
+        _conex_pool.reserve(pool_size);
+        IOTA_LOG_DEBUG(m_logger, "pool size " << pool_size);
+      }
+      catch (std::exception& e) {
+        IOTA_LOG_ERROR(m_logger, "Error in config, bad pool size, use default");
+        pool_size = SIZE_POOL;
+      }
+    }
+
     if (storage.HasMember(iota::store::types::REPLICA_SET.c_str())) {
       _replica = storage[iota::store::types::REPLICA_SET.c_str()].GetString();
-      PION_LOG_DEBUG(m_logger, "in storage replica " << _replica);
+      IOTA_LOG_DEBUG(m_logger, "in storage replica " << _replica);
     }else{
       //PORT is only used for non replica
       if (storage.HasMember(iota::store::types::PORT.c_str())) {
@@ -103,7 +112,7 @@ void iota::MongoConnection::reconnect(){
         _host.append(storage[iota::store::types::PORT.c_str()].GetString());
       }
       else {
-        PION_LOG_DEBUG(m_logger, "in storage no port defined, using 27017 by default");
+        IOTA_LOG_DEBUG(m_logger, "in storage no port defined, using 27017 by default");
         _host.append(":");
         _host.append("27017");
       }
@@ -116,12 +125,12 @@ void iota::MongoConnection::reconnect(){
         _timeout = boost::lexical_cast<double>(timeoutSTR);
       }
       catch (std::exception& e) {
-        PION_LOG_ERROR(m_logger, "Error in config, bad timeuot defined, use default");
+        IOTA_LOG_ERROR(m_logger, "Error in config, bad timeuot defined, use default");
         _timeout = MONGO_TIMEOUT;
       }
     }
     else {
-      PION_LOG_DEBUG(m_logger,
+      IOTA_LOG_DEBUG(m_logger,
                      "in storage no timeout defined, using 0 by default");
       _timeout = MONGO_TIMEOUT;
     }
@@ -130,7 +139,7 @@ void iota::MongoConnection::reconnect(){
       _database = storage[iota::store::types::DBNAME.c_str()].GetString();
     }
     else {
-      PION_LOG_DEBUG(m_logger, "in storage no dbname defined, using iot by default");
+      IOTA_LOG_DEBUG(m_logger, "in storage no dbname defined, using iot by default");
       _database = "iot";
     }
 
@@ -138,7 +147,7 @@ void iota::MongoConnection::reconnect(){
       _usuario = storage[iota::store::types::USER.c_str()].GetString();
     }
     else {
-      PION_LOG_DEBUG(m_logger, "in storage no user defined, mongo without auth");
+      IOTA_LOG_DEBUG(m_logger, "in storage no user defined, mongo without auth");
       _usuario = "";
     }
 
@@ -146,57 +155,21 @@ void iota::MongoConnection::reconnect(){
       _password = storage[iota::store::types::PWD.c_str()].GetString();
     }
     else {
-      PION_LOG_DEBUG(m_logger, "in storage no pwd defined, mongo without auth");
+      IOTA_LOG_DEBUG(m_logger, "in storage no pwd defined, mongo without auth");
       _password = "";
     }
     db_info.assign(" mongodb=" + _host + "/" + _database);
 
-
-
-    if (!_replica.empty()) {
-      PION_LOG_DEBUG(m_logger, "Replica is defined " <<  _replica);
-      std::vector<mongo::HostAndPort> hosts;
-
-      boost::char_separator<char> sep(",");
-      boost::tokenizer< boost::char_separator<char> > tokens(_host, sep);
-      BOOST_FOREACH(const std::string& t, tokens) {
-        PION_LOG_DEBUG(m_logger,t);
-        hosts.push_back(mongo::HostAndPort(t));
+    mongo::DBClientBase * con;
+    bool no_empty_pool  =true;
+    int num_conns=0;
+    for (int i=0; no_empty_pool && i < pool_size; i++){
+      con = createConnection();
+      if (con != NULL){
+        no_empty_pool = _conex_pool.push(con);
+        num_conns++;
       }
-
-      PION_LOG_DEBUG(m_logger,
-                     "Conex Mongo Replica set " <<
-                     _host << ":" <<
-                     "/" << _database << " " <<  _usuario <<
-                     "timeout in seconds:" << _timeout);
-      mongo::DBClientReplicaSet *rpSet = new mongo::DBClientReplicaSet(_replica, hosts, _timeout);
-      rpSet->connect();
-      _conex_pool = rpSet;
     }
-    else {
-      PION_LOG_DEBUG(m_logger,
-                     "Conex Mongo DBClientConnection " <<
-                     _host <<
-                     "/" << _database << " " <<  _usuario <<
-                     "timeout in seconds:" << _timeout);
-
-      mongo::DBClientConnection *conn = new mongo::DBClientConnection(true, 0,_timeout);
-      conn->connect(_host);
-      _conex_pool = conn;
-    }
-
-    if (!_usuario.empty()){
-        std::string errmsg;
-        if (_conex_pool->auth(_database, _usuario, _password,errmsg)){
-            PION_LOG_ERROR(m_logger,
-                " Mongodb auth ok user: " << _usuario);
-        }else{
-           PION_LOG_ERROR(m_logger,
-             "Error in authenticate Conexion MongoDB " << errmsg);
-        }
-
-    }
-
     iota::Alarm::info(types::ALARM_CODE_NO_MONGO, _replica + " " + _host,
                       types::INFO, "MongoConnection OK");
   }
@@ -210,74 +183,91 @@ void iota::MongoConnection::reconnect(){
   }
 }
 
-void iota::MongoConnection::reconnect(const std::string& ip,
-                                       const std::string& puerto,
-                                       const std::string& dbname,
-                                       const std::string& user,
-                                       const std::string& pwd) {
-  _host.assign(ip);
-  if (!puerto.empty()){
-     _host.append(";");
-     _host.append(puerto);
-  }
+mongo::DBClientBase * iota::MongoConnection::createConnection()
+{
+      mongo::DBClientBase *res = NULL;
 
-  _database = dbname;
-  _usuario = user;
-  _password = pwd;
+      if (!_replica.empty()) {
+      IOTA_LOG_DEBUG(m_logger, "Replica is defined " <<  _replica);
+      std::vector<mongo::HostAndPort> hosts;
 
-  try {
-    PION_LOG_DEBUG(m_logger,
-                   "Conex Mongo DBClientConnection " << _host <<
-                   "/" << _database << " " <<  _usuario);
+      boost::char_separator<char> sep(",");
+      boost::tokenizer< boost::char_separator<char> > tokens(_host, sep);
+      BOOST_FOREACH(const std::string& t, tokens) {
+        IOTA_LOG_DEBUG(m_logger,t);
+        hosts.push_back(mongo::HostAndPort(t));
+      }
 
-    mongo::DBClientConnection *conn = new mongo::DBClientConnection(true, 0,_timeout);
-    conn->connect(_host);
-    _conex_pool = conn;
+      IOTA_LOG_DEBUG(m_logger,
+                     "Conex Mongo Replica set " <<
+                     _host << ":" <<
+                     "/" << _database << " " <<  _usuario <<
+                     "timeout in seconds:" << _timeout);
+      mongo::DBClientReplicaSet *rpSet = new mongo::DBClientReplicaSet(_replica, hosts, _timeout);
+      rpSet->connect();
+      res = rpSet;
+    }
+    else {
+      IOTA_LOG_DEBUG(m_logger,
+                     "Conex Mongo DBClientConnection " <<
+                     _host <<
+                     "/" << _database << " " <<  _usuario <<
+                     "timeout in seconds:" << _timeout);
 
+      mongo::DBClientConnection *conn = new mongo::DBClientConnection(true, 0,_timeout);
+      conn->connect(_host);
+      res = conn;
+    }
 
     if (!_usuario.empty()){
         std::string errmsg;
-        if (_conex_pool->auth(_database, _usuario, _password,errmsg)){
-            PION_LOG_ERROR(m_logger,
+        if (res->auth(_database, _usuario, _password,errmsg)){
+            IOTA_LOG_ERROR(m_logger,
                 " Mongodb auth ok user: " << _usuario);
         }else{
-           PION_LOG_ERROR(m_logger,
+           IOTA_LOG_ERROR(m_logger,
              "Error in authenticate Conexion MongoDB " << errmsg);
         }
 
     }
 
-    iota::Alarm::info(types::ALARM_CODE_NO_MONGO, _host,
-                      types::INFO, "MongoConnection OK");
-  }
-  catch (mongo::DBException& exc) {
-    iota::Alarm::error(types::ALARM_CODE_NO_MONGO, _host,
-                       types::ERROR, exc.what());
-    throw iota::IotaException(iota::types::RESPONSE_MESSAGE_DATABASE_ERROR+
-                              " [" + _host + "]", exc.what(),
-                              iota::types::RESPONSE_CODE_RECEIVER_INTERNAL_ERROR);
-  }
-
+    return res;
 };
 
 iota::MongoConnection::~MongoConnection() {
-
-   if (_conex_pool != NULL){
-       delete _conex_pool;
-       _conex_pool = NULL;
-   }
+    boost::mutex::scoped_lock lock(_m);
+    mongo::DBClientBase *con;
+    while (_conex_pool.pop(con)){
+      if (con != NULL){
+        delete con;
+      }
+    }
 };
 
 mongo::DBClientBase* iota::MongoConnection::conn() {
+  boost::mutex::scoped_lock lock(_m);
   mongo::DBClientBase* conexion = 0;
 
-  if (_conex_pool == 0) {
-     reconnect();
+  if (!_conex_pool.pop(conexion)){
+    IOTA_LOG_ERROR(m_logger,
+             "It has reached the maximum mongo pool");
   }
 
-  conexion = _conex_pool;
-
+  if (conexion == 0) {
+    IOTA_LOG_ERROR(m_logger,  "create a new con");
+     conexion = createConnection();
+  }
+  IOTA_LOG_DEBUG(m_logger, "MongoConnection::conn returns " << conexion);
   return conexion;
+}
+
+ void iota::MongoConnection::done(mongo::DBClientBase* conexion) {
+  boost::mutex::scoped_lock lock(_m);
+  if (!_conex_pool.push(conexion)){
+    IOTA_LOG_ERROR(m_logger,
+             "It has reached the maximum mongo pool, delete con");
+  }
+
 }
 
 std::string iota::MongoConnection::get_endpoint(){
@@ -289,12 +279,16 @@ std::string iota::MongoConnection::get_endpoint(){
 
 }
 
-bool iota::MongoConnection::is_valid() {
-  if (_conex_pool == 0) {
-    return false;
-  }else{
-    return _conex_pool->isStillConnected ();
+iota::MongoConnection* iota::MongoConnection::instance() {
+  if (pinstance == 0) {
+    pinstance = new MongoConnection();
   }
+  return pinstance;
 }
 
-
+void iota::MongoConnection::release() {
+  if (pinstance != 0) {
+    delete pinstance;
+    pinstance = 0;
+  }
+}
